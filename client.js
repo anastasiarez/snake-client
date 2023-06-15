e314a305db3cfd0a549285e3c5f116238f9660fe
Mona Lisa <anastasiia.rn@gmail.com>
2023-06-15 11:50:36 -0400

    commit

--- client.js
+++ client.js
@@ -1,23 +1,61 @@
+// establishes a connection with the game server
+
 const net = require("net");
 
 const connect = function() {
-
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541,
   });
 
-  // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Client successfully connected");
+    console.log("Name: RAM");
+
+  //   conn.write("Move: up");
+  //   setTimeout(() => {
+  //     conn.write("Move: down");
+  //   }, 100);
+  //   setTimeout(() => {
+  //     conn.write("Move: left");
+  //   }, 200);
+  //   setTimeout(() => {
+  //     conn.write("Move: right");
+  //   }, 300);
+  //   setTimeout(() => {
+  //     conn.write("Move: up");
+  //   }, 400);
+  // });
 
   conn.on("data", (data) => {
     console.log("Server says: ", data);
   });
 
   return conn;
-};
+});
 
 console.log("Connecting ...");
 connect();
 
 module.exports = connect;
+
+//REFACTOR
+
+/**
+ * conn.on("connect", () => {
+    console.log("Client successfully connected");
+    console.log("Name: RAM");
+    const movements = ["up", "down", "left", "right", "up"];
+    let delay = 1000;
+
+    movements.forEach((movement) => {
+      setTimeout(() => {
+        conn.write(`Move: ${movement}`);
+      }, delay);
+      delay += 1000;
+    });
+  });
+
+ */
--- play.js
+++ play.js
@@ -1,6 +1,15 @@
 const {conn, setEncoding, on} = require("./client.js");
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
 
-// establishes a connection with the game server
 
