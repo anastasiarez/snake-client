3fc9c66f6a73fe3cfc12dfea64449dd2841daace
Mona Lisa <anastasiia.rn@gmail.com>
2023-06-16 13:00:33 -0400

    commit

--- input.js
+++ input.js
@@ -15,25 +15,22 @@ const handleUserInput = function(data) {
   if (data === '\u0003') { // Ctrl+C - terminate the game
     process.exit();
 
-  } else if (data === '\u001B\u005B\u0041') { // Arrow Up
+    //Used ASCII control characters instead of WASD
+
+  } else if (data === '\u001B\u005B\u0041') { // Arrow Up 
     connection.write("Move: up");
     connection.write("Say: Faster!");
-    //console.log('Move Up');
 
   } else if (data === '\u001B\u005B\u0044') { // Arrow Left
     connection.write("Move: left");
     connection.write("Say: Cookies!");
-    //console.log('Move Left');
 
   } else if (data === '\u001B\u005B\u0043') { // Arrow Right 
     connection.write("Move: right");
-    //connection.write("Say: My pressures!");
-    //console.log('Move Right');
 
   } else if (data === '\u001B[B') { // Arrow Down
     connection.write("Move: down");
-    //connection.write("Say: Got it!");
-    //console.log('Move Down');
+    connection.write("Say: Mine!");
   }
 };
 
